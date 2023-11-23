77b33c89535993370f6d33ff3c5faa2ef6c8907d
simardua <83427437+simardua@users.noreply.github.com>
2023-11-23 18:00:35 +0530

    Updated

--- firebase.js
+++ firebase.js
@@ -5,12 +5,7 @@ const GoogleAuthProvider=require('firebase/auth');
 const {getStorage,ref, uploadBytes}=require('firebase/storage')
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDup0GLTCC1lh5EdCh7Huwxq-L-qHK_jZ4",
-  authDomain: "major-de7cb.firebaseapp.com",
-  projectId: "major-de7cb",
-  storageBucket: "major-de7cb.appspot.com",
-  messagingSenderId: "669303032134",
-  appId: "1:669303032134:web:a1893e7935cd5838f83d47"
+  your_API_key
 };
 
 
